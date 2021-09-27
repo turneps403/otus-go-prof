0157f18bf3bef7ac09cec57bdbf8573341aca29c
turneps403 <turneps403@mail.ru>
2021-09-27 18:55:33 +0300

    - working multithreading version

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,11 @@
 package hw04lrucache
 
+import "sync"
+
+var (
+	mapMutex = sync.RWMutex{}
+)
+
 type Key string
 
 type Cache interface {
@@ -17,14 +23,61 @@ type lruCache struct {
 }
 
 type cacheItem struct {
-	key   string
+	key   Key
 	value interface{}
 }
 
 func NewCache(capacity int) Cache {
-	return &lruCache{
+	return Cache(&lruCache{
 		capacity: capacity,
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
+	})
+}
+
+func (lru *lruCache) Set(key Key, value interface{}) bool {
+	wasInCache := false
+	mapMutex.RLock()
+	li, ok := lru.items[key]
+	mapMutex.RUnlock()
+	if ok {
+		// remove if exists
+		lru.queue.Remove(li)
+		mapMutex.Lock()
+		delete(lru.items, key)
+		mapMutex.Unlock()
+		wasInCache = true
 	}
+	// add to head
+	el := cacheItem{key: key, value: value}
+	li = lru.queue.PushFront(el)
+	mapMutex.Lock()
+	lru.items[key] = li
+	mapMutex.Unlock()
+	if lru.queue.Len() > lru.capacity {
+		// remove the oldest
+		li := lru.queue.Back()
+		lru.queue.Remove(li)
+		mapMutex.Lock()
+		delete(lru.items, li.Val.(cacheItem).key)
+		mapMutex.Unlock()
+	}
+	return wasInCache
+}
+
+func (lru *lruCache) Get(key Key) (interface{}, bool) {
+	mapMutex.RLock()
+	li, ok := lru.items[key]
+	mapMutex.RUnlock()
+	if ok {
+		ci := li.Val.(cacheItem)
+		lru.Set(ci.key, ci.value)
+		return ci.value, true
+	}
+	return nil, false
+}
+
+func (lru *lruCache) Clear() {
+	lru.queue = NewList()
+	lru.items = make(map[Key]*ListItem, lru.capacity)
 }
--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -1,4 +1,4 @@
-package hw04lrucache
+package hw04lrucache_test
 
 import (
 	"math/rand"
@@ -7,11 +7,12 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	hw04lrucache "github.com/turneps403/otus-go-prof/hw04lrucache"
 )
 
 func TestCache(t *testing.T) {
 	t.Run("empty cache", func(t *testing.T) {
-		c := NewCache(10)
+		c := hw04lrucache.NewCache(10)
 
 		_, ok := c.Get("aaa")
 		require.False(t, ok)
@@ -21,7 +22,7 @@ func TestCache(t *testing.T) {
 	})
 
 	t.Run("simple", func(t *testing.T) {
-		c := NewCache(5)
+		c := hw04lrucache.NewCache(5)
 
 		wasInCache := c.Set("aaa", 100)
 		require.False(t, wasInCache)
@@ -55,23 +56,23 @@ func TestCache(t *testing.T) {
 }
 
 func TestCacheMultithreading(t *testing.T) {
-	t.Skip() // Remove me if task with asterisk completed.
+	//t.Skip() // Remove me if task with asterisk completed.
 
-	c := NewCache(10)
+	c := hw04lrucache.NewCache(10)
 	wg := &sync.WaitGroup{}
 	wg.Add(2)
 
 	go func() {
 		defer wg.Done()
 		for i := 0; i < 1_000_000; i++ {
-			c.Set(Key(strconv.Itoa(i)), i)
+			c.Set(hw04lrucache.Key(strconv.Itoa(i)), i)
 		}
 	}()
 
 	go func() {
 		defer wg.Done()
 		for i := 0; i < 1_000_000; i++ {
-			c.Get(Key(strconv.Itoa(rand.Intn(1_000_000))))
+			c.Get(hw04lrucache.Key(strconv.Itoa(rand.Intn(1_000_000))))
 		}
 	}()
 
