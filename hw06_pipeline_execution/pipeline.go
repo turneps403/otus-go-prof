829b3ef6dc06325aaa7d90b2cf11f14b51eb3b19
turneps403 <turneps403@mail.ru>
2021-11-11 19:21:54 +0300

    - clarifying code and linters check

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,54 +8,34 @@ type (
 
 type Stage func(in In) (out Out)
 
+func ExecutePipeline(in In, done In, stages ...Stage) Out {
+	out := in
+	for _, s := range stages {
+		out = channelWrap(out, done)
+		out = s(out)
+	}
+	return out
+}
+
 func convHook(c chan interface{}) <-chan interface{} {
 	return c
 }
 
-func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// chan in the middle
-	// tmp := make(chan interface{})
-	// go func() {
-	// 	defer close(tmp)
-	// 	for {
-	// 		select {
-	// 		case v, ok := <-in:
-	// 			if !ok {
-	// 				fmt.Println("in not OK, return")
-	// 				return
-	// 			}
-	// 			fmt.Printf("send value %v from in to tmp\n", v)
-	// 			tmp <- v
-	// 		case <-done:
-	// 			fmt.Printf("got signal from done\n")
-	// 			return
-	// 		}
-	// 	}
-	// }()
-
-	//out := convHook(tmp)
-	out := in
-	for _, s := range stages {
-		tmp := make(chan interface{})
-		go func(in In) {
-			defer close(tmp)
-			for {
-				select {
-				case v, ok := <-in:
-					if !ok {
-						// fmt.Println("in not OK, return")
-						return
-					}
-					// fmt.Printf("send value %v from in to tmp\n", v)
-					tmp <- v
-				case <-done:
-					// fmt.Printf("got signal from done\n")
+func channelWrap(in In, done In) Out {
+	tmp := make(chan interface{})
+	go func(in In) {
+		defer close(tmp)
+		for {
+			select {
+			case v, ok := <-in:
+				if !ok {
 					return
 				}
+				tmp <- v
+			case <-done:
+				return
 			}
-		}(out)
-		out = convHook(tmp)
-		out = s(out)
-	}
-	return out
+		}
+	}(in)
+	return convHook(tmp)
 }
