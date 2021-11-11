bea87fbc13f9058e22db5a81e05ca75d7b8420fc
turneps403 <turneps403@mail.ru>
2021-11-11 18:53:11 +0300

    - working pipeline

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,54 @@ type (
 
 type Stage func(in In) (out Out)
 
+func convHook(c chan interface{}) <-chan interface{} {
+	return c
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	// chan in the middle
+	// tmp := make(chan interface{})
+	// go func() {
+	// 	defer close(tmp)
+	// 	for {
+	// 		select {
+	// 		case v, ok := <-in:
+	// 			if !ok {
+	// 				fmt.Println("in not OK, return")
+	// 				return
+	// 			}
+	// 			fmt.Printf("send value %v from in to tmp\n", v)
+	// 			tmp <- v
+	// 		case <-done:
+	// 			fmt.Printf("got signal from done\n")
+	// 			return
+	// 		}
+	// 	}
+	// }()
+
+	//out := convHook(tmp)
+	out := in
+	for _, s := range stages {
+		tmp := make(chan interface{})
+		go func(in In) {
+			defer close(tmp)
+			for {
+				select {
+				case v, ok := <-in:
+					if !ok {
+						// fmt.Println("in not OK, return")
+						return
+					}
+					// fmt.Printf("send value %v from in to tmp\n", v)
+					tmp <- v
+				case <-done:
+					// fmt.Printf("got signal from done\n")
+					return
+				}
+			}
+		}(out)
+		out = convHook(tmp)
+		out = s(out)
+	}
+	return out
 }
