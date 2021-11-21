f580d831b7eb69a66a764ac245ccebedc75419ca
turneps403 <turneps403@mail.ru>
2021-11-21 22:35:20 +0300

    - initial version of executor

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	proc := exec.Command(cmd[0], cmd[1:]...)
+
+	proc.Env = make([]string, len(os.Environ()))
+	copy(proc.Env, os.Environ())
+	for k, v := range env {
+		if v.NeedRemove {
+			proc.Env = append(proc.Env, fmt.Sprintf("%v=", k))
+		} else {
+			proc.Env = append(proc.Env, fmt.Sprintf("%v=%v", k, v))
+		}
+	}
+
+	proc.Stdout = os.Stdout
+	proc.Stdin = os.Stdin
+	proc.Stderr = os.Stderr
+
+	err := proc.Run()
+	if err != nil {
+		return err.(*exec.ExitError).ExitCode()
+	}
+	return 0
 }
