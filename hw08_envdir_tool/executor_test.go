b3854f8fc5caf84818ce0d122c65b926eb99a104
turneps403 <turneps403@mail.ru>
2021-11-22 17:05:49 +0300

    - remove game with stdout

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -9,23 +8,12 @@ import (
 
 func TestRunGit(t *testing.T) {
 	t.Log("run git --version")
-	stdout := os.Stdout
-	defer func() { os.Stdout = stdout }()
-	os.Stdout = os.NewFile(0, os.DevNull)
 	exCode := RunCmd([]string{"git", "--version"}, make(Environment))
 	require.Equal(t, 0, exCode)
 }
 
 func TestRunCmdFailArg(t *testing.T) {
 	t.Log("run git -KBD")
-	stdout := os.Stdout
-	stderr := os.Stderr
-	defer func() {
-		os.Stdout = stdout
-		os.Stderr = stderr
-	}()
-	os.Stdout = os.NewFile(0, os.DevNull)
-	os.Stderr = os.NewFile(0, os.DevNull)
 	exCode := RunCmd([]string{"git", "--KBD"}, make(Environment))
 	require.Equal(t, true, exCode == 129)
 }
@@ -39,9 +27,6 @@ func TestRunCmdPanic(t *testing.T) {
 		}
 	}()
 
-	stdout := os.Stdout
-	defer func() { os.Stdout = stdout }()
-	os.Stdout = os.NewFile(0, os.DevNull)
 	e := make(Environment)
 	e["FOO"] = EnvValue{Value: "foo" + string('\x00') + "bar"}
 	exCode := RunCmd([]string{"git", "--version"}, e)
