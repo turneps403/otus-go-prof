6abe4bc5d4db927beef088669620ec888b38f064
turneps403 <turneps403@mail.ru>
2021-11-22 17:03:05 +0300

    - test

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -7,27 +7,17 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestRunPwdEnv(t *testing.T) {
-	t.Log("run env")
-	//stdout := os.Stdout
-	//defer func() { os.Stdout = stdout }()
-	//os.Stdout = os.NewFile(0, os.DevNull)
-	//exCode := RunCmd([]string{"pwd"}, make(Environment))
+func TestRunGit(t *testing.T) {
+	t.Log("run git --version")
+	stdout := os.Stdout
+	defer func() { os.Stdout = stdout }()
+	os.Stdout = os.NewFile(0, os.DevNull)
 	exCode := RunCmd([]string{"git", "--version"}, make(Environment))
 	require.Equal(t, 0, exCode)
 }
 
-func TestRunCmdLsLa(t *testing.T) {
-	t.Log("run ls -la")
-	stdout := os.Stdout
-	defer func() { os.Stdout = stdout }()
-	os.Stdout = os.NewFile(0, os.DevNull)
-	exCode := RunCmd([]string{"ls", "-l"}, make(Environment))
-	require.Equal(t, 0, exCode)
-}
-
 func TestRunCmdFailArg(t *testing.T) {
-	t.Log("run ls -KBD")
+	t.Log("run git -KBD")
 	stdout := os.Stdout
 	stderr := os.Stderr
 	defer func() {
@@ -36,12 +26,12 @@ func TestRunCmdFailArg(t *testing.T) {
 	}()
 	os.Stdout = os.NewFile(0, os.DevNull)
 	os.Stderr = os.NewFile(0, os.DevNull)
-	exCode := RunCmd([]string{"ls", "-KBD"}, make(Environment))
-	require.Equal(t, 1, exCode)
+	exCode := RunCmd([]string{"git", "--KBD"}, make(Environment))
+	require.Equal(t, true, exCode == 129)
 }
 
 func TestRunCmdPanic(t *testing.T) {
-	t.Log("run broken env")
+	t.Log("run panic")
 
 	defer func() {
 		if r := recover(); r == nil {
@@ -54,6 +44,6 @@ func TestRunCmdPanic(t *testing.T) {
 	os.Stdout = os.NewFile(0, os.DevNull)
 	e := make(Environment)
 	e["FOO"] = EnvValue{Value: "foo" + string('\x00') + "bar"}
-	exCode := RunCmd([]string{"env"}, e)
+	exCode := RunCmd([]string{"git", "--version"}, e)
 	require.Equal(t, -1, exCode) // never happens
 }
