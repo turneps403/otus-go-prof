0d7675c189e576fe5ef0147804c85a11792bc0bf
turneps403 <turneps403@mail.ru>
2021-12-09 21:29:22 +0300

    - revert package prefix

--- hw10_program_optimization/stats_optimization_test.go
+++ hw10_program_optimization/stats_optimization_test.go
@@ -35,7 +35,7 @@ func TestGetDomainStat_Time_And_Memory(t *testing.T) {
 		require.NoError(t, err)
 
 		b.StartTimer()
-		stat, err := GetDomainStat(data, "biz")
+		stat, err := stats.GetDomainStat(data, "biz")
 		b.StopTimer()
 		require.NoError(t, err)
 
